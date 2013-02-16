0fcff55ed777799891dfeb4a1bdf59d43a006f33
Robert Collins <robertc@robertcollins.net>
2013-02-16 22:44:00 +1300

    Refresh of define-stream-result

--- testtools/testresult/real.py
+++ testtools/testresult/real.py
@@ -262,7 +262,7 @@ class StreamResult(object):
     generally make a sequence of calls like::
 
       >>> result.status(self.id(), 'inprogress')
-      >>> result.status(self.id(), 'finish')
+      >>> result.status(self.id(), 'success')
 
     General concepts
     ----------------
