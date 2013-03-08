5149daf234492af5023e6f4623548f6b3f7ae72f
Robert Collins <robertc@robertcollins.net>
2013-03-09 00:22:35 +1300

    Refresh of detect-incomplete-tests

--- testtools/__init__.py
+++ testtools/__init__.py
@@ -11,6 +11,7 @@ __all__ = [
     'ExpectedException',
     'ExtendedToOriginalDecorator',
     'FixtureSuite',
+    'IncompleteTestDetector',
     'iterate_tests',
     'MultipleExceptions',
     'MultiTestResult',
@@ -74,6 +75,7 @@ else:
     from testtools.testresult import (
         CopyStreamResult,
         ExtendedToOriginalDecorator,
+        IncompleteTestDetector,
         MultiTestResult,
         StreamFailFast,
         StreamResult,
