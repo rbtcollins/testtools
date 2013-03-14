11b04f75c42c91af43b0f0abed44462045f1ad02
Robert Collins <robertc@robertcollins.net>
2013-03-15 05:25:17 +1300

    Refresh of test-control

--- testtools/testresult/real.py
+++ testtools/testresult/real.py
@@ -403,16 +403,22 @@ class CopyStreamResult(StreamResult):
 
 
 class StreamFailFast(StreamResult):
-    """Call the supplied callback if an error is seen in a stream."""
+    """Call the supplied callback if an error is seen in a stream.
+    
+    An example callback::
+    
+       def do_something():
+           pass
+    """
 
-    def __init__(self, callback):
-        self.callback = callback
+    def __init__(self, on_error):
+        self.on_error = on_error
 
     def status(self, test_id=None, test_status=None, test_tags=None,
         runnable=True, file_name=None, file_bytes=None, eof=False,
         mime_type=None, route_code=None, timestamp=None):
         if test_status in ('uxsuccess', 'fail'):
-            self.callback()
+            self.on_error()
 
 
 class StreamToDict(StreamResult):
